f88cffa3a97d1fb3216d1901f7e6d7de39e580a1
Maël Chanon <95433353+MaelChanon@users.noreply.github.com>
2023-12-04 12:18:44 +0100

    Feature/profile upload (#11)
    * first try upload image
    
    * save
    
    * Création de la route /users/uploadPreviewProfile
    
    * save
    
    * test & correction

---
+++ test/unit/infrastructure/repositories/DocumentRepository.test.js
@@ -0,0 +1,28 @@
+const DocumentRepositoryTest = require("../../../../lib/infrastructure/repositories/DocumentRepository")
+const createUser = require("../../../../lib/application/use_cases/user/CreateUser");
+let filePath
+const documentRepository = new DocumentRepositoryTest();
+const mockFile = {
+    hapi: {
+        filename: "test.png"
+    },
+    _data: "testDFata"
+}
+describe('document repository',  ()=>{
+    describe('document repository with no problem', ()=>{
+        it('should create file without problem', async () =>{
+            filePath=  await documentRepository.uploadFile("test/unit/infrastructure/repositories/testFolder", mockFile)
+        })
+        it('should creadzadte file without problem', async () =>{
+            documentRepository.deleteFile(filePath)
+        })
+    })
+    describe('document repository with errors', () =>{
+        it("shouldn't create file",  async () =>{
+            await expect(documentRepository.uploadFile("test/unit/infrastructure/repositories/testFolder",{})).rejects.toThrow('Cannot read property \'filename\' of undefined')
+        })
+        it("shouldn't delete file",  async () =>{
+            await expect(documentRepository.uploadFile("test/unit/infrastructure/repositories/testFolder/test.png",{})).rejects.toThrow('Cannot read property \'filename\' of undefined')
+        })
+    })
+})
