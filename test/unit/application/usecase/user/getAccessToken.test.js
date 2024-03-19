e4b7a037448db5db5f260c37dea19fe5d3830a7c
Maël Chanon <95433353+MaelChanon@users.noreply.github.com>
2024-03-19 17:34:29 +0100

    replaced email by user in signin (#43)

--- test/unit/application/usecase/user/getAccessToken.test.js
+++ test/unit/application/usecase/user/getAccessToken.test.js
@@ -18,8 +18,25 @@ describe('getAccessToken', () =>{
             photo_temporaire:"path/to/file",
             password:await bcrypt.hash(passwordTest,10),
             token:"token",
+            is_private: false,
             id_role:1,
             ban_until:new Date("10-06-2003"),
+        }
+        const expectedAccessToken = {
+            user: {
+                id_utilisateur:"id",
+                pseudo:"pseudo",
+                email:"email",
+                alias:"alias",
+                bio:"bio",
+                photo:"path/to/file",
+                photo_temporaire:"path/to/file",
+                type: 'user',
+                is_private: false,
+                id_role:1,
+                ban_until:new Date("10-06-2003"),
+            },
+            token: 1
         }
         const mockUserRepository = new UserRepository();
         const mockAccessTokenManager = {};
@@ -34,7 +51,7 @@ describe('getAccessToken', () =>{
                     accessTokenManager: mockAccessTokenManager
                 }
             )
-        ).toBe(1)
+        ).toEqual(expectedAccessToken)
     })
 
 
