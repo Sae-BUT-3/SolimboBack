e799bfa05c2024931e977656d44903646117a32b
Maël <chanon.mael@gmail.com>
2024-04-13 15:53:04 +0200

    test correction

--- test/unit/application/usecase/user/getUserByPseudo.test.js
+++ test/unit/application/usecase/user/getUserByPseudo.test.js
@@ -1,45 +1,49 @@
-const getUserBuPseudo = require('../../../../../lib/application/use_cases/user/getUserByPseudo')
+require("dotenv").config();
+const getUserBuPseudo = require("../../../../../lib/application/use_cases/user/getUserByPseudo");
 const mockUser = {
-    id_utilisateur:1,
-    pseudo:'pseudo',
-    alias:'alias',
-    email:'testemail@gmail.com',
-    photo:'path/to/file',
-    photo_temporaire: 'path/to/file',
-    token:'token',
-    refresh_token:'refreshToken',
-    password:'password',
-    id_role:1,
-    ban_until:null,
-    confirmed:false,
-    confirm_token:'fezfezgezrhgez',
-    type:'user',
-}
+  id_utilisateur: 1,
+  pseudo: "pseudo",
+  alias: "alias",
+  email: "testemail@gmail.com",
+  photo: "path/to/file",
+  photo_temporaire: "path/to/file",
+  token: "token",
+  refresh_token: "refreshToken",
+  password: "password",
+  id_role: 1,
+  ban_until: null,
+  confirmed: false,
+  confirm_token: "fezfezgezrhgez",
+  type: "user",
+};
 const expectedUser = {
-    id_utilisateur:1,
-    pseudo:'pseudo',
-    alias:'alias',
-    email:'testemail@gmail.com',
-    photo:'path/to/file',
-    photo_temporaire: 'path/to/file',
-    id_role:1,
-    ban_until:null,
-    type:'user',
-}
-const mockUserRepository = {}
-describe('getUserByPseudo',()=>{
-    afterEach(async () => {
-        jest.clearAllMocks();
+  id_utilisateur: 1,
+  pseudo: "pseudo",
+  alias: "alias",
+  email: "testemail@gmail.com",
+  photo: `${process.env.API_URL}/path/to/file`,
+  photo_temporaire: `${process.env.API_URL}/path/to/file`,
+  id_role: 1,
+  ban_until: null,
+  type: "user",
+};
+const mockUserRepository = {};
+describe("getUserByPseudo", () => {
+  afterEach(async () => {
+    jest.clearAllMocks();
+  });
+  it("should return Public User", async () => {
+    mockUserRepository.getByEmailOrPseudo = jest.fn(() => mockUser);
+    const result = await getUserBuPseudo("test", {
+      userRepository: mockUserRepository,
     });
-    it('should return Public User',async ()=>{
-        console.log("test")
-        mockUserRepository.getByEmailOrPseudo = jest.fn(()=>mockUser)
-        const result = await getUserBuPseudo("test",{userRepository:mockUserRepository})
-        expect(result).toEqual(expectedUser)
-    })
-    it('should return null',async ()=>{
-        mockUserRepository.getByEmailOrPseudo = jest.fn(()=>null)
-        const result = await getUserBuPseudo("test",{userRepository:mockUserRepository})
-        expect(result).toBeNull()
-    })
-})
+    expect(result).toEqual(expectedUser);
+  });
+  it("should return null", async () => {
+    mockUserRepository.getByEmailOrPseudo = jest.fn(() => null);
+    const result = await getUserBuPseudo("test", {
+      userRepository: mockUserRepository,
+    });
+    expect(result).toBeNull();
+  });
+});
