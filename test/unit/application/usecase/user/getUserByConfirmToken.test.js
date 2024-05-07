8ed7e83d797c895f47d8de0f5c9eb87165a2c42e
Maël Chanon <95433353+MaelChanon@users.noreply.github.com>
2024-05-07 15:41:32 +0200

    Feature/user page (#56)
    * save
    
    * save
    
    * correction
    
    * added is private
    
    * modify profile
    
    * save
    
    * .env
    
    * test correction
    
    * correction on getTracks
    
    * save
    
    * save
    
    * save
    
    * correction
    
    * added is private
    
    * modify profile
    
    * save
    
    * .env
    
    * test correction
    
    * correction on getTracks
    
    * save

--- test/unit/application/usecase/user/getUserByConfirmToken.test.js
+++ test/unit/application/usecase/user/getUserByConfirmToken.test.js
@@ -1,45 +1,49 @@
-const getUserByConfirmToken = require('../../../../../lib/application/use_cases/user/getUserByConfirmToken')
+const getUserByConfirmToken = require("../../../../../lib/application/use_cases/user/getUserByConfirmToken");
 const mockUser = {
-    id_utilisateur:1,
-    pseudo:'pseudo',
-    alias:'alias',
-    email:'testemail@gmail.com',
-    photo:'path/to/file',
-    photo_temporaire: 'path/to/file',
-    token:'token',
-    refresh_token:'refreshToken',
-    password:'password',
-    id_role:1,
-    ban_until:null,
-    confirmed:false,
-    confirm_token:'fezfezgezrhgez',
-    type:'user',
-}
+  id_utilisateur: 1,
+  pseudo: "pseudo",
+  alias: "alias",
+  email: "testemail@gmail.com",
+  photo: "path/to/file",
+  photo_temporaire: "path/to/file",
+  token: "token",
+  refresh_token: "refreshToken",
+  password: "password",
+  id_role: 1,
+  ban_until: null,
+  confirmed: false,
+  confirm_token: "fezfezgezrhgez",
+  type: "user",
+};
 const expectedUser = {
-    id_utilisateur:1,
-    pseudo:'pseudo',
-    alias:'alias',
-    email:'testemail@gmail.com',
-    photo:'path/to/file',
-    photo_temporaire: 'path/to/file',
-    id_role:1,
-    ban_until:null,
-    type:'user',
-}
-const mockUserRepository = {}
-describe('getUserByPseudo',()=>{
-    afterEach(async () => {
-        jest.clearAllMocks();
+  id_utilisateur: 1,
+  pseudo: "pseudo",
+  alias: "alias",
+  email: "testemail@gmail.com",
+  photo: `${process.env.API_URL}/path/to/file`,
+  photo_temporaire: `${process.env.API_URL}/path/to/file`,
+  id_role: 1,
+  ban_until: null,
+  type: "user",
+};
+const mockUserRepository = {};
+describe("getUserByPseudo", () => {
+  afterEach(async () => {
+    jest.clearAllMocks();
+  });
+  it("should return Public User", async () => {
+    console.log("test");
+    mockUserRepository.getByConfirmToken = jest.fn(() => mockUser);
+    const result = await getUserByConfirmToken("test", {
+      userRepository: mockUserRepository,
     });
-    it('should return Public User',async ()=>{
-        console.log("test")
-        mockUserRepository.getByConfirmToken = jest.fn(()=>mockUser)
-        const result = await getUserByConfirmToken("test",{userRepository:mockUserRepository})
-        expect(result).toEqual(expectedUser)
-    })
-    it('should return null',async ()=>{
-        mockUserRepository.getByConfirmToken = jest.fn(()=>null)
-        const result = await getUserByConfirmToken("test",{userRepository:mockUserRepository})
-        expect(result).toBeNull()
-    })
-})
+    expect(result).toEqual(expectedUser);
+  });
+  it("should return null", async () => {
+    mockUserRepository.getByConfirmToken = jest.fn(() => null);
+    const result = await getUserByConfirmToken("test", {
+      userRepository: mockUserRepository,
+    });
+    expect(result).toBeNull();
+  });
+});
